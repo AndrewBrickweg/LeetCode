5d17eb749f3419632ef1c0cd5407efd91df8a6c6
Andrew Brickweg <Andrewcbrickweg@icloud.com>
2024-11-04 16:27:29 -0500

    #1-TwoSum-Using Hashmap

---
+++ PythonSolutions/TwoSum_1.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+
+        hashMap = {}
+
+        for i, val in enumerate(nums):
+            complement = target - val
+
+            if complement in hashMap:
+                return[i, hashMap[complement]]
+            hashMap[val] = i
